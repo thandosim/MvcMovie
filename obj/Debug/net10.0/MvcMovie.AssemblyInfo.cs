1ad3ee7ced27c6e026cd774679f2239452cb4887
thandosim <sim24001@byui.edu>
2025-11-05 12:52:41 +0200

    Update source link and binary files for MvcMovie project
    - Updated sourcelink JSON to point to the latest commit.
    - Modified apphost.exe and MvcMovie.dll binaries.
    - Updated cache files for Razor and RJS.

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0ff7edfa06892d05313f1d3f4b6ad13a3ec031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0feeafedd75cc9d2991f9c98a6e73be81ad5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
