6c4ffc23272493d8adc39397651591940113c204
thandosim <sim24001@byui.edu>
2025-11-05 12:40:00 +0200

    Add initial migration for Movie entity with properties
    - Created migration files for the initial database schema.
    - Defined the Movie table with columns: Id, Title, ReleaseDate, Genre, Price, and Rating.
    - Set Id as the primary key with auto-increment.

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a5cb2863c47640c617c99e4938b4af71216386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0ff7edfa06892d05313f1d3f4b6ad13a3ec031")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
