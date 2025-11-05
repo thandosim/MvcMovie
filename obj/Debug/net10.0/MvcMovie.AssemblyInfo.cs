5c0ff7edfa06892d05313f1d3f4b6ad13a3ec031
thandosim <sim24001@byui.edu>
2025-11-05 12:24:06 +0200

    added search funtcion to movies view

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b75202b8d567971913b6abbeaca845ee843f64d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a5cb2863c47640c617c99e4938b4af71216386")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
