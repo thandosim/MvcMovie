6334b348c7cfa5f175c3556bcb1807b6c70cd14c
thandosim <sim24001@byui.edu>
2025-11-05 13:08:42 +0200

    fixed movies/index title, added movies to db

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0feeafedd75cc9d2991f9c98a6e73be81ad5e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad3ee7ced27c6e026cd774679f2239452cb4887")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
